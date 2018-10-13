cfc958ae6be477820a61a78be1c2bc4a7c712f28
ShrutiBadekar <shrtbadekar@gmail.com>
2018-10-14 01:48:30 +0530

    report generation completed and google pie chart added in view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { UploadComponent } from './upload/upload.component';
-
+import { Ng2GoogleChartsModule } from 'ng2-google-charts';
 @NgModule({
   declarations: [
     AppComponent,
     UploadComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    Ng2GoogleChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/upload/upload.component.ts
+++ src/app/upload/upload.component.ts
@@ -16,7 +16,21 @@ export class UploadComponent implements OnInit {
   report=[];
   status: string;
   selectedFiles: FileList;
+  fileContents;
   currentFileUpload: File;
+  pieChartData =  {
+    chartType: 'PieChart',
+    dataTable: [
+      ['Task', 'Hours per Day'],
+      ['Work',     11],
+      ['Eat',      2],
+      ['Commute',  2],
+      ['Watch TV', 2],
+      ['Sleep',    7]
+    ],
+    options: {'title': 'Tasks'},
+    chartArea: {width: 900, height: 900}
+  };
   constructor(private uploadService: UploadService,private ngxXml2jsonService: NgxXml2jsonService,private es: ElasticService,private cd: ChangeDetectorRef) { }
 
   ngOnInit() {
@@ -30,8 +44,7 @@ export class UploadComponent implements OnInit {
     }).then(() => {
       this.cd.detectChanges();
     });
-  }
-  
+  }  
   selectFile(event) {
     this.selectedFiles = event.target.files;
     this.fileReader(event.target);
@@ -42,8 +55,8 @@ export class UploadComponent implements OnInit {
     var file= target.files[0];
     var myReader:FileReader = new FileReader();
     myReader.onloadend=function(e){
-      var fileContents = myReader.result;
-      self.xmlToJson(fileContents);
+      self.fileContents = myReader.result;
+      self.xmlToJson(self.fileContents);
     }
     myReader.readAsText(file);
   }
@@ -65,29 +78,35 @@ export class UploadComponent implements OnInit {
   //  console.log(xmlFileContents['vehicles'].vehicle[0]);
    var vehicleList=xmlFileContents['vehicles'].vehicle;
    var vehicleDetail={};
+   var timeStamp = new Date().toLocaleString();
    vehicleList.forEach(element => {
      console.log(element);
-     
+     var powerTrain=self.powerTrainValue(element);
+     var wheels=self.wheelsValue(element);     
+     var frame=element.frame.material;
+     var vehicleName=self.nameOfvehicle(frame,powerTrain,wheels);
      vehicleDetail={
+       "name":vehicleName,
        "id":element.id,
-       "name":element.id,
-       "frame":element.frame.material
+       "frame":frame,
+       "powerTrain":powerTrain,
+       "wheels":wheels,
+       "timestamp":timeStamp
      }
-     self.report.push(element);
+     self.report.push(vehicleDetail);
    });
-    //this.storeData(xmlFileContents);
-
-       
+    // this.storeData(xmlFileContents,timeStamp);       
   }
   
-  storeData(xmlFileContents){
+  storeData(xmlFileContents,timeStamp){
     this.es.addToIndex({
       index: 'vehicle_tracker',
       type: 'vehicles',
       id:'_' + Math.random().toString(36).substr(2, 9),
+      submitted: timeStamp,
       body: {
         xmlFileContents:xmlFileContents,
-        submitted: new Date().toLocaleString()
+        submitted: timeStamp
       }
     }).then((result) => {
       console.log(result);
@@ -97,5 +116,63 @@ export class UploadComponent implements OnInit {
       console.error(error);
     });
   }
-
+  powerTrainValue(element){
+    var powerTrain;
+    var powerTrainJson=element.powertrain;
+      if(powerTrainJson.hasOwnProperty('human')){
+        powerTrain='Human';
+      }else if(powerTrainJson.hasOwnProperty('Internal combustion')){
+        powerTrain='Internal Combustion';
+      }
+      else if(powerTrainJson.hasOwnProperty('Bernoulli')){
+        powerTrain='Bernoulli';
+      }
+    return powerTrain;
+  }
+  wheelsValue(element){
+    var wheelsArray=element.wheels.wheel;
+    var wheelsValue="";
+    var count=0;
+    if(!wheelsArray){
+      wheelsValue="none";
+    }else{
+      var wheelsArrayLength=wheelsArray.length;
+      wheelsValue=wheelsArrayLength+" "+wheelsArray[wheelsArrayLength-1].material+"(";
+      
+        wheelsArray.forEach(element => {          
+          if(count===wheelsArrayLength-1){
+            wheelsValue=wheelsValue+element.position;
+          }else{
+            count++;
+            wheelsValue=wheelsValue+element.position+", ";
+          }
+        });
+        wheelsValue=wheelsValue+")";      
+    }    
+    return wheelsValue;
+  }
+  nameOfvehicle(frame,powerTrain,wheels){
+    var nameOfvehicle;
+    if(wheels === "none" && frame === "plastic"){
+      nameOfvehicle="Hang Glider";
+    }
+    else if(frame === "plastic"){
+      nameOfvehicle="Big Wheel";
+    }
+    else if(frame === "metal" && powerTrain === "Human"){
+      nameOfvehicle="Bicycle";
+    }
+    else if(frame === "metal" && powerTrain === "Internal Combustion"){
+      var numberOfWheels=wheels.split(" ");
+      if(numberOfWheels[0]===2){
+        nameOfvehicle="Motorcycle";
+      }else{
+        nameOfvehicle="Car";
+      }
+    }
+    return nameOfvehicle;
+  }
+  onClickMe(){
+    document.write(this.fileContents);
+  }
 }

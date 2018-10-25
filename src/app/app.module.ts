2b0db631b1757566099e574e40ec911bb3f2fd40
ShrutiBadekar <shrtbadekar@gmail.com>
2018-10-26 00:24:38 +0530

    header component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { UploadComponent } from './upload/upload.component';
 import { Ng2GoogleChartsModule } from 'ng2-google-charts';
 import { HistoryComponent } from './history/history.component';
 import 'rxjs/add/operator/toPromise';
+import { HeaderComponent } from './header/header.component';
 @NgModule({
   declarations: [
     AppComponent,
     UploadComponent,
-    HistoryComponent
+    HistoryComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
